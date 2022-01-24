affc3fbf767111511da6e78a5c359356c731c49f
lrakotoarivony <lucas.rakotoarivony@etu.unice.fr>
2022-01-24 12:37:46 -0500

    Little change

--- cyber/simulation/tabular_q_learning/run.py
+++ cyber/simulation/tabular_q_learning/run.py
@@ -37,7 +37,7 @@ def default_config() -> ClientConfig:
     :return: Default configuration for the experiment
     """
     q_agent_config = QAgentConfig(gamma=0.999, alpha=0.05, epsilon=1, render=False, eval_sleep=0.9,
-                                  min_epsilon=0.01, eval_episodes=100, train_log_frequency=1,
+                                  min_epsilon=0.01, eval_episodes=10, train_log_frequency=1,
                                   epsilon_decay=0.9999, video=True, eval_log_frequency=1,
                                   video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=40001,
                                   eval_render=False, gifs=True, gif_dir=default_output_dir() + "/gifs",
