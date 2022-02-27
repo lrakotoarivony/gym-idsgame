efe2c5cee0451e98003c33852a6c7c59790d7a90
Lucas Rakotoarivony <34739719+lrakotoarivony@users.noreply.github.com>
2022-02-27 11:40:59 +0100

    Update simulation_tabular.py

--- simulation/simulation_tabular.py
+++ simulation/simulation_tabular.py
@@ -22,9 +22,11 @@ def default_config() -> ClientConfig:
     :return: Default configuration for the experiment
     """
     simulation_config = SimulationConfig(render=True, sleep=0.8, video=False, log_frequency=1,
-                                         video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=10,
+                                         video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=100,
                                          gifs=False, gif_dir=default_output_dir() + "/gifs", video_frequency = 1)
-    q_agent_config = QAgentConfig(attacker_load_path="/home/kali/Documents/projet_3A/gym-idsgame/simulation/data/1645706033.8600655_attacker_q_table.npy",defender_load_path="/home/kali/Documents/projet_3A/gym-idsgame/cyber/data/1645706033.8600655_defender_q_table.npy")
+    #q_agent_config = QAgentConfig(attacker_load_path="/home/kali/Documents/projet_3A/gym-idsgame/simulation/model/attacker_tabular.npy",defender_load_path="/home/kali/Documents/projet_3A/gym-idsgame/simulation/model/defender_tabular.npy")
+    q_agent_config = QAgentConfig(attacker_load_path="/home/kali/Documents/projet_3A/gym-idsgame/simulation/model/attacker_only_new_env.npy",defender_load_path="/home/kali/Documents/projet_3A/gym-idsgame/simulation/model/defender_only_new_env.npy")
+
     env_name = "idsgame-cyber-v0"
     #AgentType.RANDOM.value
     #AgentType.DEFEND_MINIMAL_VALUE.value
@@ -33,8 +35,24 @@ def default_config() -> ClientConfig:
                                  simulation_config=simulation_config, output_dir=default_output_dir(),
                                  title="TabularQAgentAttacker vs TabularQAgentDefender",
                                  q_agent_config=q_agent_config)'''
+    #two bots hack proba 0.6
+    #one defender hack proba 0.61
+    #one attacker hack proba 0.53
+    #two learner hack proba 0.49
+
+    #only attacker train 0.57
+    #only defender train 0.51
+
+    #new
+    #two bots hack proba 0.55
+    #one defender hack proba 0.61
+    #one attacker hack proba 0.53
+    #two learner hack proba 0.49
+
+    #only attacker train 0.63
+    #only defender train 0.53 (not true)
     client_config = ClientConfig(env_name=env_name, attacker_type=AgentType.ATTACK_MAXIMAL_VALUE.value,
-                                 defender_type=AgentType.DEFEND_MINIMAL_VALUE.value, mode=RunnerMode.SIMULATE.value,
+                                 defender_type=AgentType.TABULAR_Q_AGENT.value, mode=RunnerMode.SIMULATE.value,
                                  simulation_config=simulation_config, output_dir=default_output_dir(),
                                  title="TabularQAgentAttacker vs TabularQAgentDefender",
                                  q_agent_config=q_agent_config)
