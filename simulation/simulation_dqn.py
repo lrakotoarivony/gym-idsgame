2e6431e676491f2e43f977b672d5547d00253bc1
lrakotoarivony <lucas.rakotoarivony@etu.unice.fr>
2022-03-01 04:28:59 -0500

    Mise au propre simu dqn

--- simulation/simulation_dqn.py
+++ simulation/simulation_dqn.py
@@ -11,6 +11,7 @@ import gym
 from gym_idsgame.agents.training_agents.q_learning.dqn.dqn import DQNAgent
 from gym_idsgame.agents.training_agents.q_learning.dqn.dqn_config import DQNConfig
 import torch
+import argparse
 
 
 
@@ -22,14 +23,23 @@ def default_output_dir() -> str:
     script_dir = os.path.dirname(__file__)
     return script_dir
 
+def define_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--attacker_path', type=str,default='')
+    parser.add_argument('--defender_path', type=str,default='')
+    parser.add_argument('--num_episodes', type=int, default = 100)
 
-def default_config() -> ClientConfig:
+    args = parser.parse_args()
+    return args
+
+
+def default_config(args) -> ClientConfig:
     """
     :return: Default configuration for the experiment
     """
-    simulation_config = SimulationConfig(render=True, sleep=0.8, video=True, log_frequency=1,
-                                         video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=2,
-                                         gifs=True, gif_dir=default_output_dir() + "/gifs", video_frequency = 1)
+    simulation_config = SimulationConfig(render=True, sleep=0.8, video=False, log_frequency=1,
+                                         video_fps=5, video_dir=default_output_dir() + "/videos", num_episodes=1,
+                                         gifs=False, gif_dir=default_output_dir() + "/gifs", video_frequency = 1)
 
     dqn_config = DQNConfig(input_dim=40, attacker_output_dim=32, defender_output_dim=40, hidden_dim=16,
                            replay_memory_size=10000,
@@ -39,7 +49,7 @@ def default_config() -> ClientConfig:
                            loss_fn="Huber", optimizer="Adam", lr_exp_decay=True, lr_decay_rate=0.9999)
 
     q_agent_config = QAgentConfig(gamma=0.999, alpha=0.00001, epsilon=1, render=True, eval_sleep=0.9,
-                                  min_epsilon=0.01, eval_episodes=100, train_log_frequency=100,
+                                  min_epsilon=0.01, eval_episodes=args.num_episodes, train_log_frequency=100,
                                   epsilon_decay=0.9999, video=True, eval_log_frequency=1,
                                   video_fps=5, video_dir=default_output_dir() + "/results/videos", num_episodes=20001,
                                   eval_render=False, gifs=True, gif_dir=default_output_dir() + "/results/gifs",
@@ -47,37 +57,34 @@ def default_config() -> ClientConfig:
                                   save_dir=default_output_dir() + "/results/data", dqn_config=dqn_config,
                                   checkpoint_freq=5000)
     env_name = "idsgame-cyber-v0"
-    #q_agent_config = QAgentConfig(attacker_load_path="/home/kali/Documents/projet_3A/gym-idsgame/cyber/simulation/results/data/1643884050.5795374_attacker_q_network.pt",defender_load_path="/home/kali/Documents/projet_3A/gym-idsgame/cyber/simulation/results/data/1643884050.5795374_defender_q_network.pt")
     client_config = ClientConfig(env_name=env_name, attacker_type=AgentType.DQN_AGENT.value,
                                  defender_type=AgentType.DQN_AGENT.value, mode=RunnerMode.SIMULATE.value,
                                  simulation_config=simulation_config, output_dir=default_output_dir(),
                                  title="TabularQAgentAttacker vs TabularQAgentDefender",
                                  q_agent_config=q_agent_config)
-                                 #initial_state_path = default_output_dir() + "/initial_state/initial_state.pkl")
     return client_config
 
 
 # Program entrypoint
 if __name__ == '__main__':
-    
-    config = default_config()
+    args = define_args()
+    config = default_config(args)
     time_str = str(time.time())
     util.create_artefact_dirs(config.output_dir,1)
     logger = util.setup_logger("idsgame-v0-dqn_vs_dqn", config.output_dir + "/results/logs/",
                                time_str=time_str)
     config.logger = logger
     config.simulation_config.logger = logger
-    config.simulation_config.to_csv(config.output_dir + "/results/hyperparameters/" + time_str + ".csv")
 
     env = gym.make(config.env_name, idsgame_config = config.idsgame_config,
                        save_dir=config.output_dir + "/results/data/" + str(config.random_seed),
                        initial_state_path = config.initial_state_path)
     agent = DQNAgent(env, config.q_agent_config)
     
-    agent.attacker_q_network.load_state_dict(torch.load('/home/kali/Documents/projet_3A/new/gym-idsgame/simulation/results/data/1646063764.165759_attacker_q_network.pt'))
+    agent.attacker_q_network.load_state_dict(torch.load(args.attacker_path))
     agent.attacker_q_network.eval()
 
-    agent.defender_q_network.load_state_dict(torch.load('/home/kali/Documents/projet_3A/new/gym-idsgame/simulation/results/data/1646063764.165759_defender_q_network.pt'))
+    agent.defender_q_network.load_state_dict(torch.load(args.defender_path))
     agent.defender_q_network.eval()
 
 
